3aff30fa96bfaaa72070518b30c3104d81e02ad8
Spyynox <maxime.mbiya-kiaku@supinternet.fr>
2021-11-01 14:53:15 +0100

    reglage de beug mineur

--- tests/RecipeApiTest.php
+++ tests/RecipeApiTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests;
 
-use Faker\Factory;
 use App\Tests\AbstractEndPoint;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -25,22 +24,14 @@ class RecipeApiTest extends AbstractEndPoint
     public function testApiRecipePost(): void
     {
         $response = $this->getResponseFromRequest(Request::METHOD_POST, '/api/recipes',
-            $this->getPayload()
+            $this->userPayload
         );
         $responseContent = $response->getContent();
         $responseDecoded = json_decode($responseContent);
 
-        dd($responseContent);
 
         $this->assertResponseIsSuccessful();
         static::assertJson($responseContent);
         static::assertNotEmpty($responseDecoded);
-    }
-
-    private function getPayload(): string
-    {
-        $faker = Factory::create();
-
-        return sprintf($this->userPayload, $faker->name);
     }
 }
